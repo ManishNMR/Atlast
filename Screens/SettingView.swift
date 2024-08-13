a9f08937860c313d9239dbcc4e2b50a1fef2c2c2
Manish-NMR <2016nmr2016@gmail.com>
2024-08-13 23:26:53 +0530

    :grinning: SettingView Update

---
+++ Screens/SettingView.swift
@@ -0,0 +1,19 @@
+//
+//  SettingView.swift
+//  iNews
+//
+//  Created by user2 on 02/02/24.
+//
+
+import SwiftUI
+
+struct SettingView: View {
+    
+    var body: some View {
+        Text("HELLO WORLd")
+    }
+}
+
+#Preview {
+    SettingView()
+}
